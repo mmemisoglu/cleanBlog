549e593eec8a63f43ac357a450f4637fe1b6bd05
Mustafa Memişoğlu <m.memisoglu04@gmail.com>
2023-02-13 02:45:05 +0300

    Ödev-7 teslim

---
+++ app.mjs
@@ -0,0 +1,17 @@
+import express from "express";
+import nodemon from "nodemon";
+
+const app = express();
+app.get("/", (req, res) => {
+  const blog = {
+    id: 1,
+    title: "Blog title",
+    description: "Blog description",
+  };
+  res.send(blog);
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda çalışıyor.`);
+});
