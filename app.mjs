d5a20ec28ae6f2e51349d043125302e56d3b904a
Mustafa Memişoğlu <m.memisoglu04@gmail.com>
2023-02-13 06:25:34 +0300

    Ödev-8 yapıldı.

--- app.mjs
+++ app.mjs
@@ -1,14 +1,29 @@
 import express from "express";
 import nodemon from "nodemon";
+import path from "path";
+import ejs from "ejs";
 
 const app = express();
+
+//Template Engine
+app.set("view engine", "ejs");
+
+//Middleware
+const __dirname = path.resolve;
+app.use(express.static("public"));
+
+//Routes
 app.get("/", (req, res) => {
-  const blog = {
-    id: 1,
-    title: "Blog title",
-    description: "Blog description",
-  };
-  res.send(blog);
+  res.render("index");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/post", (req, res) => {
+  res.render("post");
+});
+app.get("/add_post", (req, res) => {
+  res.render("add_post");
 });
 
 const port = 3000;
