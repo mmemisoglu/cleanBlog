759f386829fba26b620eeaeb445f4b25af383b00
Mustafa Memişoğlu <m.memisoglu04@gmail.com>
2023-02-14 06:49:14 +0300

    Ödev-10 teslim

--- app.mjs
+++ app.mjs
@@ -26,6 +26,12 @@ app.get("/", async (req, res) => {
     blogs
   });
 });
+app.get("/post/:id", async (req, res) => {
+  const blog = await Blog.findById(req.params.id)
+  res.render('post',{
+    blog
+  });
+});
 app.get("/about", (req, res) => {
   res.render("about");
 });
